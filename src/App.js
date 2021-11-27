997a6dae7b8a0626056df082083adfeac16bc382
Vivek Verma <virtualiz.me@gmail.com>
2021-11-27 16:30:23 +0530

    updated template for `-v4.0.4`

--- src/App.js
+++ src/App.js
@@ -1,58 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { NavBar, NavBarLink, SystemThemeFollower, TransitionToLeft } from 'react-windows-ui'
+import { AppTheme, SplashScreen, NavBar, NavBarLink, TransitionToLeft } from 'react-windows-ui'
 import Page0 from './pages/page0'
 import Page1 from './pages/page1'
 import Page2 from './pages/page2'
+import Img from './assets/mountain.jpg'
 
 const App = () => {
-    return (
-        <Router basename="react-windows-ui">
 
-          <TransitionToLeft />
-          {/*for registering right page transition*/}
+  const [splash, setSplash] = useState(true);
 
-          <SystemThemeFollower/>
+  useEffect(() => {
+   setSplash(false);
+  }, [])
 
-          <NavBar 
-            title="App Boilderplate"
-            titleShort={<i className="icons10-windows"></i>}
-          //titleShort="R" render text or icons
-          //mobileHasIcons={true}
-            shadowOnScroll={true}>
+  return (
+    <Router basename="react-windows-ui">
 
-            <h1>Pages</h1>
-            <div className="app-hr"></div>
+      <SplashScreen
+        //duration={1000}
+        isVisible={splash}
+        title={"react-windows-ui-boilerplate"}
+      />
 
-            <NavBarLink
-              to="/"
-              exact={true}
-              text="Home"
-              icon={<i className="icons10-home"></i>}
-            />
+      <AppTheme
+        scheme="system"
+      />
 
-            <NavBarLink
-              to="/page1"
-              text="Page1"
-              icon={<i className="icons10-grid-2"></i>}
-            />
+      <TransitionToLeft />
+      {/*for registering right page transition*/}
 
-            <NavBarLink
-              to="/page2"
-              text="Page2"
-              icon={<i className="icons10-columns"></i>}
-            />
+      <NavBar 
+        title="App Boilderplate"
+      //mobileHasIcons={true}
+        shadowOnScroll={true}>
 
-          </NavBar>
+        <h1>Pages</h1>
+        <div className="app-hr"></div>
 
-          <Switch>
-            <Route path="/" component={Page0}  exact />
-            <Route path="/page1" component={Page1} />
-            <Route path="/page2" component={Page2} />
-          </Switch>
+        <NavBarLink
+          to="/"
+          exact={true}
+          text="Home"
+          img={Img}
+        />
 
-        </Router>
-    )
-  }
+        <NavBarLink
+          to="/page1"
+          text="Page1"
+          icon={<i className="icons10-grid-2"></i>}
+        />
+
+        <NavBarLink
+          to="/page2"
+          text="Page2"
+          icon={<i className="icons10-columns"></i>}
+        />
+
+      </NavBar>
+
+      <Switch>
+        <Route path="/" component={Page0}  exact />
+        <Route path="/page1" component={Page1} />
+        <Route path="/page2" component={Page2} />
+      </Switch>
+
+    </Router>
+  )
+}
     
-export default App
+export default App;
